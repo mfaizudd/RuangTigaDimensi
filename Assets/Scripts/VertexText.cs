07e258d14b17d564fb6cd852836535808cc6e406
mfaizudd <mfaizudd@gmail.com>
2021-04-21 15:31:15 +0700

    feat: vertex text gaps

--- Assets/Scripts/VertexText.cs
+++ Assets/Scripts/VertexText.cs
@@ -1,9 +1,10 @@
-using System;
 using TMPro;
 using UnityEngine;
 
 public class VertexText : MonoBehaviour
 {
+    [SerializeField] private float gap = 1f;
+    
     private bool _isFollowTargetNull;
     private TextMeshPro _textMesh;
     private Transform _followTarget;
@@ -29,7 +30,9 @@ public class VertexText : MonoBehaviour
         if (_isFollowTargetNull)
             return;
 
-        transform.position = FollowTarget.position;
+        var targetPosition = FollowTarget.position;
+        var direction = (targetPosition - Vector3.zero).normalized;
+        transform.position = targetPosition + direction * gap;
 
     }
 
