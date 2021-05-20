3e6f6b20b6a3e08a7bb2b27ccd2bfe1699f011d7
mfaizudd <mfaizudd@gmail.com>
2021-05-20 17:39:30 +0700

    fix: update content object collection

--- Assets/Scripts/Data/ContentObjectCollection.cs
+++ Assets/Scripts/Data/ContentObjectCollection.cs
@@ -6,8 +6,15 @@ namespace Data
     [CreateAssetMenu(fileName = "New Content Collection", menuName = "Content/Content Object Collection", order = 0)]
     public class ContentObjectCollection : ScriptableObject
     {
-        [SerializeField] private List<ContentObject> content;
+        [SerializeField] private List<ContentObject> contents;
 
-        public List<ContentObject> Content => content;
+        public void ShowContent(string key, Vector3 location, Geometry geometry)
+        {
+            var content = contents.Find(c => c.Key == key);
+            if (content == null) return;
+
+            var instance = Instantiate(content.ContentPrefab, location, Quaternion.identity);
+            instance.Inject(geometry);
+        }
     }
 }
