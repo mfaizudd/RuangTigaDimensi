ba0917fc7bae170733b0b12314e103acebcf9408
mfaizudd <mfaizudd@gmail.com>
2021-05-24 13:27:11 +0700

    fix: update multi triangle's cleanup
    because the previous one is supposed to handle only one

--- Assets/Scripts/SpawnRightTriangleGeometryAction.cs
+++ Assets/Scripts/SpawnRightTriangleGeometryAction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -7,7 +8,7 @@ public class SpawnRightTriangleGeometryAction : GeometryAction
     [SerializeField] private Line linePrefab;
     [SerializeField] private Triangle[] triangles;
 
-    private GameObject _triangle;
+    private readonly List<GameObject> _triangleObjects = new List<GameObject>();
 
     public override void Invoke(Geometry geometry)
     {
@@ -25,28 +26,29 @@ public class SpawnRightTriangleGeometryAction : GeometryAction
             if (aVertex is null || bVertex is null || cVertex is null)
                 return;
 
-            _triangle = new GameObject("Triangle");
-            _triangle.transform.SetParent(geometry.transform);
+            var triangleInstance = new GameObject("Triangle");
+            _triangleObjects.Add(triangleInstance);
+            triangleInstance.transform.SetParent(geometry.transform);
             var aPosition = aVertex.transform.position;
             var bPosition = bVertex.transform.position;
             var cPosition = cVertex.transform.position;
         
-            var lineB = Instantiate(linePrefab, _triangle.transform);
+            var lineB = Instantiate(linePrefab, triangleInstance.transform);
             lineB.LocalPointA = cPosition + cPosition.normalized * 0.1f;
             lineB.LocalPointB = aPosition + aPosition.normalized * 0.1f;
         
-            var lineA = Instantiate(linePrefab, _triangle.transform);
+            var lineA = Instantiate(linePrefab, triangleInstance.transform);
             lineA.LocalPointA = cPosition + cPosition.normalized * 0.1f;
             lineA.LocalPointB = bPosition + bPosition.normalized * 0.1f;
 
             var aDirection = (lineA.LocalPointB - lineA.LocalPointA).normalized;
             var bDirection = (lineB.LocalPointB - lineB.LocalPointA).normalized;
         
-            var lineSquareA = Instantiate(linePrefab, _triangle.transform);
+            var lineSquareA = Instantiate(linePrefab, triangleInstance.transform);
             lineSquareA.LocalPointA = lineA.LocalPointA + aDirection;
             lineSquareA.LocalPointB = lineSquareA.LocalPointA + bDirection;
         
-            var lineSquareB = Instantiate(linePrefab, _triangle.transform);
+            var lineSquareB = Instantiate(linePrefab, triangleInstance.transform);
             lineSquareB.LocalPointA = lineSquareA.LocalPointB;
             lineSquareB.LocalPointB = lineSquareB.LocalPointA + -aDirection;
         }
@@ -54,7 +56,10 @@ public class SpawnRightTriangleGeometryAction : GeometryAction
 
     public override void Cleanup()
     {
-        Destroy(_triangle.gameObject);
+        foreach (var triangle in _triangleObjects)
+        {
+            Destroy(triangle);
+        }
     }
     
     [Serializable]
