2652cb3c409c225eee0dc39928253b9aac8ad97c
mfaizudd <mfaizudd@gmail.com>
2021-05-24 13:44:27 +0700

    fix: update ExtendedEditor.cs
    to allows cached property draw

--- Assets/Scripts/EnsignBandeng/Editor/ExtendedEditor.cs
+++ Assets/Scripts/EnsignBandeng/Editor/ExtendedEditor.cs
@@ -8,32 +8,52 @@ namespace EnsignBandeng.Editor
     public class ExtendedEditor<T> : UnityEditor.Editor where T : UnityEngine.Object
     {
         protected T Target => target as T;
-        private SerializedProperty GetProperty(string name) => serializedObject.FindProperty(name);
+        private SerializedProperty GetProperty(string propertyName) => serializedObject.FindProperty(propertyName);
 
         protected void PrefixLabel(string label)
         {
             EditorGUILayout.PrefixLabel(label);
         }
+        
+        protected void DrawProperty(SerializedProperty property)
+        {
+            EditorGUILayout.PropertyField(property);
+        }
+
+        protected void DrawProperty(SerializedProperty property, string label)
+        {
+            EditorGUILayout.PropertyField(property, new GUIContent(label));
+        }
+
+        protected void DrawToggle(SerializedProperty property)
+        {
+            property.boolValue = EditorGUILayout.Toggle(property.boolValue);
+        }
+
+        protected void DrawIntField(SerializedProperty property)
+        {
+            property.intValue = EditorGUILayout.IntField(property.intValue);
+        }
     
-        protected void DrawProperty(string name)
+        protected void DrawProperty(string propertyName)
         {
-            EditorGUILayout.PropertyField(GetProperty(name));
+            EditorGUILayout.PropertyField(GetProperty(propertyName));
         }
 
-        protected void DrawProperty(string name, string label)
+        protected void DrawProperty(string propertyName, string label)
         {
-            EditorGUILayout.PropertyField(GetProperty(name), new GUIContent(label));
+            EditorGUILayout.PropertyField(GetProperty(propertyName), new GUIContent(label));
         }
 
-        protected void DrawToggle(string name)
+        protected void DrawToggle(string propertyName)
         {
-            var prop = GetProperty(name);
+            var prop = GetProperty(propertyName);
             prop.boolValue = EditorGUILayout.Toggle(prop.boolValue);
         }
 
-        protected void DrawIntField(string name)
+        protected void DrawIntField(string propertyName)
         {
-            var prop = GetProperty(name);
+            var prop = GetProperty(propertyName);
             prop.intValue = EditorGUILayout.IntField(prop.intValue);
         }
 
